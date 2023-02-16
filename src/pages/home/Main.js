165152bbf71c5b51beabac3a1c6b95a0c3994675
matohoundev <matohoundev@gmail.com>
2023-02-16 17:36:11 +0100

    add-modal

--- src/pages/home/Main.js
+++ src/pages/home/Main.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+// data
 import { states } from "../../data/states";
+// redux
 import { addEmployee } from "../../redux/actions/employeeActions";
+// plugins
 import Select from "react-select";
+import Modal from "react-modal";
 
 const Main = () => {
   const dispatch = useDispatch();
+
   const dateOfDay = new Date().toISOString().slice(0, 10);
+  const [modalOpen, setModalOpen] = useState(false);
+
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
@@ -37,6 +44,24 @@ const Main = () => {
     if (selected === "department") {
       setDepartment(e.value);
     }
+  };
+
+  const handleOpenModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.75)",
+    },
   };
 
   const saveEmployee = (e) => {
@@ -143,12 +168,19 @@ const Main = () => {
             options={optionsForDepartment}
             onChange={(e) => handleChange(e, "department")}
           />
-          <input type="submit" value="Save" className="btnSubmit" />
+          <button type="submit" onClick={handleOpenModal} className="btnSubmit">
+            Save
+          </button>
         </form>
       </div>
-      <div id="confirmation" className="modal">
-        Employee Created!
-      </div>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={handleOpenModal}
+        style={customStyles}
+        contentLabel="Modal save employee"
+      >
+        <p>Employee Created !</p>
+      </Modal>
     </main>
   );
 };
