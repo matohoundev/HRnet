804a70cfe4f183ed9d35fbc1ac63883ec99aca1c
matohoundev <matohoundev@gmail.com>
2023-02-17 16:01:22 +0100

    create modal plugins and delete react-modal

---
+++ src/components/modal.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const defaultStyle = {
+  position: "fixed",
+  top: "0",
+  left: "0",
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "rgba(0, 0, 0, 0.75)",
+};
+
+const defaultContentStyle = {
+  backgroundColor: "#ffffff",
+  padding: "20px",
+  borderRadius: "5px",
+};
+
+const Modal = ({
+  isOpen,
+  onRequestClose,
+  style = {},
+  contentStyle = {},
+  children,
+}) => {
+  const modalStyle = { ...defaultStyle, ...style };
+  const modalContentStyle = { ...defaultContentStyle, ...contentStyle };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div style={modalStyle} onClick={onRequestClose}>
+      <div style={modalContentStyle} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
